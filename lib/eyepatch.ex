9205fed03fb6b2605bfe8e9047cb4c6d07a976cb
Fabian Muscariello <nroi@mailbox.org>
2019-03-02 11:27:48 +0100

    Import skeleton files from mix

---
+++ lib/eyepatch.ex
@@ -0,0 +1,18 @@
+defmodule Eyepatch do
+  @moduledoc """
+  Documentation for Eyepatch.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eyepatch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eyepatch_test.exs
@@ -0,0 +1,8 @@
+defmodule EyepatchTest do
+  use ExUnit.Case
+  doctest Eyepatch
+
+  test "greets the world" do
+    assert Eyepatch.hello() == :world
+  end
+end
